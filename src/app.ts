e2bba5c6a9bbf231ee060bf5853b8aba7d47b290
Ruslan Khuzhin <khuzh.n@gmail.com>
2021-12-04 22:22:32 +0300

    update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,23 @@
 import fastify from 'fastify';
-import plugins from './plugins';
-import modules from './modules';
+import { plugins } from './plugins';
+import { api } from './api';
 
-const server = fastify({ logger: true });
+const server = fastify({
+  logger: {
+    prettyPrint: {
+      levelFirst: true,
+    },
+  },
+});
 
 server.register(plugins);
-server.register(modules);
+server.register(api);
 
 const start = async () => {
   try {
-    await server.listen(8000);
-  } catch (err) {
-    server.log.error(err);
+    await server.listen(process.env.PORT || 8000);
+  } catch (error) {
+    server.log.error(error);
     process.exit(1);
   }
 };
